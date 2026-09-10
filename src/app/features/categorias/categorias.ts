0d73dc8e913aaf94890b3282ce0b4367ae234b50
gustavo79246-boop <gustavo79246@gmail.com>
2026-09-10 08:22:40 -0300

    categorias atualizada
    categorias feitas

--- src/app/features/categorias/categorias.ts
+++ src/app/features/categorias/categorias.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterLink } from '@angular/router';
+
+interface Livro {
+  id: number;
+  titulo: string;
+  autor: string;
+  capa: string;
+}
+
+interface Categoria {
+  nome: string;
+  livros: Livro[];
+}
 
 @Component({
   selector: 'app-categorias',
@@ -10,4 +21,27 @@ import { RouterLink } from '@angular/router';
   templateUrl: './categorias.html',
   styleUrl: './categorias.css',
 })
-export class CategoriasComponent {}
+export class CategoriasComponent {
+  categorias: Categoria[] = [
+    {
+      nome: 'Tecnologia & Programação',
+      livros: [
+        { id: 1, titulo: 'Entendendo Algoritmos', autor: 'Aditya Y. Bhargava', capa: 'img/algoritmos.jpg' },
+        { id: 2, titulo: 'Angular 17 do Zero', autor: 'Daniel Schmitz', capa: 'img/angular.jpg' },
+        { id: 3, titulo: 'TI - Tec. Da Informação', autor: 'Aditya Y. Bhargava', capa: 'img/ti.jpg' }
+      ]
+    },
+    {
+      nome: 'Inteligência Artificial',
+      livros: [
+        { id: 4, titulo: 'Inteligência Artificial', autor: 'Kai-Fu LEE', capa: 'img/ia.jpg' }
+      ]
+    },
+    {
+      nome: 'DevOps & Infraestrutura',
+      livros: [
+        { id: 5, titulo: 'Manual de DevOps', autor: 'John Allspaw', capa: 'img/devops.jpg' }
+      ]
+    }
+  ];
+}
